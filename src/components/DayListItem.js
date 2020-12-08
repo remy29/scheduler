dc7f9f88e4ea17c5b9d12965a4b9f2f081932ab8
remy29 <arthur.d.remy@gmail.com>
2020-12-07 16:44:15 -0800

    added variable styling to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,18 @@
 import React from "react";
-/* import classnames from 'classnames'; */
+
+import "components/DayListItem.scss";
+import classnames from 'classnames';
 
 export default function DayListItem(props) {
 
-  /* const ListItemClass = classnames(
-
-  ) */
+  const listItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  }
+    
+  )
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={listItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{ props.name }</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
