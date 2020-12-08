625c655f90be8c567ba42fba5b775d522c4c653e
remy29 <arthur.d.remy@gmail.com>
2020-12-07 16:23:44 -0800

    built DayListItem Component and Story

---
+++ src/components/DayListItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+/* import classnames from 'classnames'; */
+
+export default function DayListItem(props) {
+
+  /* const ListItemClass = classnames(
+
+  ) */
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{ props.name }</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
