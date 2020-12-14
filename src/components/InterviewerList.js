8606fd740e11b08b41d10fdc98f029cb0772c2c6
remy29 <arthur.d.remy@gmail.com>
2020-12-14 14:05:02 -0800

    added first test for Appoinment component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem.js";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   
@@ -36,4 +37,8 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewerListItems}</ul>
     </section>
   )
-}
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
